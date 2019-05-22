71a03ee3bfbee60b9891f8e1b6d2c7590365285e
mecozma <mecozma@gmail.com>
2019-05-22 16:50:24 +0100

    Added methods to check if there are four color matching cells in all three directions

--- assets/js/grid-class.js
+++ assets/js/grid-class.js
@@ -3,9 +3,10 @@ class Grid {
     this.ROWS = 6;
     this.COLUMNS = 7;
     this.selector = selector;
-    this.player = 'red';
+    this.player = "red";
     this.generateGrid();
     this.addEventListener();
+    this.checkForWinner();
   }
 
   generateGrid() {
@@ -49,8 +50,6 @@ class Grid {
     }
     // On mouse enter over a cell that has .column.empty classes do something
     $gameBoard.on("mouseenter", ".column.empty", function() {
-      // The row constant is assigned  the data asigned to the data-row attribute  assigned by the loop above
-      const row = $(this).data("row");
       // The column constant is assigned the data asigned to the data-column attribute by the loop above
       const column = $(this).data("column");
       const $lastEmptyCell = findLastEmptyCell(column);
@@ -63,11 +62,84 @@ class Grid {
 
     $gameBoard.on("click", ".column.empty", function() {
       const column = $(this).data("column");
+      // Last empty cell clicked on
       const $lastEmptyCell = findLastEmptyCell(column);
       $lastEmptyCell.removeClass(`empty next-${that.player}`);
       $lastEmptyCell.addClass(that.player);
-      that.player = (that.player === 'red') ? 'black' : 'red';
-      $(this).trigger('mouseenter');
+      $lastEmptyCell.data("player", that.player);
+
+      const winner = that.checkForWinner(
+        $lastEmptyCell.data("row"),
+        $lastEmptyCell.data("column")
+      );
+      //Checks if the winner is true;
+      if (winner) {
+        alert(`Game over! ${that.player} has won!`);
+        return;
+      }
+      // Changes to color of the player
+      that.player = that.player === "red" ? "black" : "red";
+      $(this).trigger("mouseenter");
     });
+  }
+
+  checkForWinner(row, column) {
+    const that = this;
+
+    function $getCell(i, j) {
+      return $(`.column[data-row='${i}'][data-column='${j}']`);
+    }
+    // Checks if there is a cell with the same color in a given direction
+    function checkDirection(direction) {
+      let total = 0;
+      let i = row + direction.i;
+      let j = column + direction.j;
+      let $next = $getCell(i, j);
+      while (
+        i >= 0 &&
+        i < that.ROWS &&
+        j >= 0 &&
+        j < that.COLUMNS &&
+        $next.data("player") === that.player
+      ) {
+        total++;
+        i += direction.i;
+        j += direction.j;
+        $next = $getCell(i, j);
+      }
+      return total;
+    }
+    // Checks if there are 4 cells with the same color
+    function checkWin(directionA, directionB) {
+      const total = 1 + checkDirection(directionA) + checkDirection(directionB);
+
+      if (total >= 4) {
+        return that.player;
+      } else {
+        return null;
+      }
+    }
+    // Checks if there is a match on the vertical cells
+    function checkVerticals() {
+      return checkWin({ i: -1, j: 0 }, { i: 1, j: 0 });
+    }
+    // Checls if there is a match on the horizontal cells
+    function checkHorizontals() {
+      return checkWin({ i: 0, j: -1 }, { i: 0, j: 1 });
+    }
+    // Checks if there is a match on diagonal Bottom Left to Top Right
+    function checkDiagonalBLtoTR() {
+      return checkWin({ i: 1, j: -1 }, { i: 1, j: 1 });
+    }
+    // Checks if there is a match on diagonal Top Left to Bottom Right
+    function checkDiagonalTLtoBR() {
+      return checkWin({ i: 1, j: 1 }, { i: -1, j: -1 });
+    }
+    return (
+      checkVerticals() ||
+      checkHorizontals() ||
+      checkDiagonalBLtoTR() ||
+      checkDiagonalTLtoBR()
+    );
   }
 }
